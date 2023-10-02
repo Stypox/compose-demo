b482b0505f9705df9fd95ff598442a7a07eaf161
Stypox <stypox@pm.me>
2023-10-02 16:08:46 +0200

    Add base Device

---
+++ app/src/main/java/org/stypox/composedemo/Device.kt
@@ -0,0 +1,8 @@
+package org.stypox.composedemo
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Device() {
+
+}
--- app/src/main/java/org/stypox/composedemo/MainActivity.kt
+++ app/src/main/java/org/stypox/composedemo/MainActivity.kt
@@ -3,13 +3,16 @@ package org.stypox.composedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import org.stypox.composedemo.ui.theme.ComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,11 +25,23 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HomeInformationPanel(
-                        iconPainter = painterResource(R.drawable.small_logo_alone),
-                        name = "Impact Hub",
-                        address = "Via Roberto da Sanseverino 95 - Trento",
-                    )
+                    Column {
+                        HomeInformationPanel(
+                            iconPainter = painterResource(R.drawable.small_logo_alone),
+                            name = "Impact Hub",
+                            address = "Via Roberto da Sanseverino 95 - Trento",
+                        )
+
+                        Divider(modifier = Modifier.padding(horizontal = 16.dp))
+
+                        Text(
+                            modifier = Modifier.padding(16.dp),
+                            text = "Available devices to configure",
+                            style = MaterialTheme.typography.labelMedium,
+                        )
+
+                        Device()
+                    }
                 }
             }
         }
